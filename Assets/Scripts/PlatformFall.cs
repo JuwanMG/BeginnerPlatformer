0df8f07dd49d2a59172d9d923f76742034bee585
JuwanMG <juwangilliam@yahoo.com>
2017-06-08 18:46:23 -0400

    Beginner Platformer Update
    Added Scripts for the Platform fall and Coin Spawn

--- Assets/Scripts/PlatformFall.cs
+++ Assets/Scripts/PlatformFall.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class PlatformFall : MonoBehaviour {
 
-    public float fallDelay;
+    public float fallDelay = 1f;
     private Rigidbody2D rb2d;
 
 
 	// Use this for initialization
 	void Awake () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 
      void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
+            Invoke("Fall", fallDelay);
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
     }
 }
